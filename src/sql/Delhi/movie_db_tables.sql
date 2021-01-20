27b4f8dcb6bc9a015631615d9bc8a35b0b2387ce
Sanjana Sunil <sanjana99sunil@gmail.com>
2021-01-20 19:51:24 +0530

    added delhi fragment creation

---
+++ src/sql/Delhi/movie_db_tables.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS Theater;
+DROP TABLE IF EXISTS Screen;
+DROP TABLE IF EXISTS Screening;
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE Theater (
+    TheaterID INT NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    NumScreens INT,
+    PRIMARY KEY (TheaterID)
+);
+
+CREATE TABLE Screen (
+    ScreenID INT NOT NULL,
+    TheaterID INT NOT NULL,
+    ScreenName VARCHAR(255) NOT NULL,
+    NumSeats INT,
+    PRIMARY KEY (ScreenID)
+);
+
+CREATE TABLE Screening (
+    ShowID INT NOT NULL,
+    MovieID INT NOT NULL,
+    ScreenID INT NOT NULL,
+    Time DATETIME NOT NULL,
+    Price FLOAT(7,2) NOT NULL,
+    PRIMARY KEY (ShowID)
+);
+
+CREATE TABLE User (
+    EMail VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    PhNo VARCHAR(15) NOT NULL,
+    DOB DATE,
+    PRIMARY KEY (EMail)
+);
