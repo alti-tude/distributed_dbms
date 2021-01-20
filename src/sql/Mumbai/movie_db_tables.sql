6784586201082359b7d1f5d5f31745c38f0d1337
Sanjana Sunil <sanjana99sunil@gmail.com>
2021-01-20 19:45:18 +0530

    added Mumbai fragment creations

---
+++ src/sql/Mumbai/movie_db_tables.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS Theater;
+DROP TABLE IF EXISTS Screen;
+DROP TABLE IF EXISTS Screening;
+DROP TABLE IF EXISTS Movie;
+
+CREATE TABLE Theater (
+    TheaterID INT NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    NumScreens INT,
+    PRIMARY KEY (TheaterID)
+);
+
+CREATE TABLE Screen (
+    ScreenID INT NOT NULL,
+    TheaterID INT NOT NULL,
+    ScreenName VARCHAR(255) NOT NULL,
+    NumSeats INT,
+    PRIMARY KEY (ScreenID)
+);
+
+CREATE TABLE Screening (
+    ShowID INT NOT NULL,
+    MovieID INT NOT NULL,
+    ScreenID INT NOT NULL,
+    Time DATETIME NOT NULL,
+    Price FLOAT(7,2) NOT NULL,
+    PRIMARY KEY (ShowID)
+);
+
+CREATE TABLE Movie (
+    MovieID INT NOT NULL,
+    Summary MEDIUMTEXT,
+    MinAllowedAge INT,
+    PRIMARY KEY (MovieID)
+);
