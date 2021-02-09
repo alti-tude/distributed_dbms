3aed76c7d819326c5b2049a628f6b03ea1719d44
alti-tude <kartik.gupta0204@gmail.com>
2021-02-10 02:17:14 +0530

    git DS init.py fix

--- src/DDBMS/DataStructures/__init__.py
+++ src/DDBMS/DataStructures/__init__.py
@@ -1,8 +1,4 @@
 from .Column import Column
 from .Table import Table
-<<<<<<< HEAD
 
 from .Symbols import Aggregation, Keywords, PredicateOps
-=======
-from .Aggregation import Aggregation
->>>>>>> eb47fbe843ecfb31afdcaed36c4b884a414b8746
