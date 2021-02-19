1a1e615d871367c745c9457fc73797d99ce0957b
alti-tude <kartik.gupta0204@gmail.com>
2021-02-19 15:11:07 +0530

    added predicate object builder test

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -1,11 +1,10 @@
-from DDBMS.DataStructures.SQLQuery import SQLQuery
-from DDBMS.DataStructures import Column, Table
-from DDBMS.DataStructures.Predicate import buildPredicateTree
+from DDBMS.Parser.SQLQuery import Column, SQLQuery, Table
 from DDBMS.RATree.RATreeBuilder import RATreeBuilder
 import DDBMS
+
 def test_getPredicateObj():
 
-    predicate = buildPredicateTree({
+    predicate = SQLQuery().newPredicate({
         'and': [
             {'gt': [Column(name="id", table=Table(name="t1")), 3]},
             {'eq': [Column(name="a", table=Table(name="t1")), Column(name="b", table=Table(name="t2"))]},
