c02958b942c2dc6f436cc348d5f9555714ae4769
alti-tude <kartik.gupta0204@gmail.com>
2021-02-20 20:45:07 +0530

    - added getAllColumns to predicate - added project before groupby in the initial ra tree

--- src/DDBMS/RATree/RATreeBuilder.py
+++ src/DDBMS/RATree/RATreeBuilder.py
@@ -9,10 +9,14 @@ class RATreeBuilder:
     def __init__(self, sql_query: SQLQuery):
         self.sql_query = sql_query
         
+        #bottom up tree
+
         self.leaves = self.buildTablesAsLeaves()
         self.joined = self.crossTables()
         self.selected = self.seperateSelect(self.sql_query.where, self.joined)
-        self.gamma_added = self.addGroupby(self.selected)
+        self.project_before_groupby = self.addProjectBeforeGroupby(self.selected)
+
+        self.gamma_added = self.addGroupby(self.project_before_groupby)
         self.having_added = self.seperateSelect(self.sql_query.having, self.gamma_added)
         self.projected = self.addProject(self.having_added)
 
@@ -37,6 +41,16 @@ class RATreeBuilder:
         
         return cur_node
     
+    def addProjectBeforeGroupby(self, cur_root):
+        required_columns = []
+        for predicate in self.sql_query.having:
+            required_columns.extend(predicate.getAllColumns())
+        
+        required_columns.extend(self.sql_query.groupby)
+        required_columns.extend(self.sql_query.select)
+
+        return ProjectNode(columns=required_columns, children=[cur_root])
+
     def addGroupby(self, cur_root):
         cur_node = cur_root
 
