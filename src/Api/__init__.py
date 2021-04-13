87f65dd5fb729febe8ad685dec1399d9f0bafa64
Sanjana Sunil <sanjana99sunil@gmail.com>
2021-04-14 04:04:58 +0530

    added blueprint for commit

--- src/Api/__init__.py
+++ src/Api/__init__.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 from .User import bp as user_bp
 from .Internal import bp as internal_bp
+from .Commit import bp as commit_bp
 
 app.register_blueprint(user_bp)
 app.register_blueprint(internal_bp)
+app.register_blueprint(commit_bp)
