34cca8bccaab3b37a273f695ce9997ec9c9eac32
Sanjana Sunil <sanjana99sunil@gmail.com>
2021-04-26 22:29:02 +0530

    added logging dir

--- src/DDBMS/CommitProtocol/__init__.py
+++ src/DDBMS/CommitProtocol/__init__.py
@@ -1,3 +1,9 @@
+import os
 import logging
+import Config
+
+logging.basicConfig(filename=os.path.join(Config.COMMIT_STATE_FOLDER, "commit_protocol.log")
+                            filemode='a',
+                            level=logging.info)
 
 logger = logging.getLogger(__name__)
